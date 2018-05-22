92d5d11a62fb2d6b23dc222d90f2beb65a64a623
k0nkupa <theton369@gmail.com>
2018-05-22 21:08:39 +1200

    Update convo.js

--- public/javascripts/convo.js
+++ public/javascripts/convo.js
@@ -1,6 +1,6 @@
 var botui = new BotUI('api-bot');
 
-var socket = io.connect('http://localhost:8010');
+var socket = io.connect(window.location.hostname);
 
 socket.removeAllListeners();
 botui.message.add({
